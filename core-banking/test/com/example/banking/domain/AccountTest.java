c0dc26c0a137549766becee53e551c7c88c32a7b
deepcloudlabs <deepcloudlabs@gmail.com>
2023-10-31 02:47:07 +0300

    Update AccountTest.java

--- core-banking/test/com/example/banking/domain/AccountTest.java
+++ core-banking/test/com/example/banking/domain/AccountTest.java
@@ -1,9 +1,11 @@
 package com.example.banking.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
