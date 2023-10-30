458875b589cc4bc70d8375a8b0924d08ffe1c76d
deepcloudlabs <deepcloudlabs@gmail.com>
2023-10-30 14:20:02 +0300

    Update BankApp.java

--- core-banking/src/com/example/banking/application/BankApp.java
+++ core-banking/src/com/example/banking/application/BankApp.java
@@ -5,14 +5,14 @@ import com.example.banking.domain.Account;
 public class BankApp {
 
 	public static void main(String[] args) {
+		// -ea -XX:-UseCompressedOops
 		Account acc = new Account("tr1", 100_000);
 		acc.deposit(10_000);
 		acc.withdraw(50_000);
 		// Error: System.out.println(acc.iban);
 		// Error: acc.balance = acc.balance - 10_000_000;
 		acc.withdraw(10_000_000);
-		
-		System.out.println(acc.getBalance());
+		System.out.println(acc.toString());
 	}
 
 }
