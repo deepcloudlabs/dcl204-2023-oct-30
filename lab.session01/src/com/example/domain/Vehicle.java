0e42e0c339c2adf3ae1e6752534918c9954f9bc8
deepcloudlabs <deepcloudlabs@gmail.com>
2023-10-31 11:10:06 +0300

    add solution

--- lab.session01/src/com/example/domain/Vehicle.java
+++ lab.session01/src/com/example/domain/Vehicle.java
@@ -1,12 +1,44 @@
 package com.example.domain;
 
 public class Vehicle {
-	public double capacity;
-	public double currentLoad;
+	private final double capacity;
+	private double currentLoad;
+
+	public Vehicle(double capacity) {
+		this.capacity = capacity;
+	}
+
 	// information hiding
 	// Constructor -> maxCapacity
 	// getter
 	// loadBox(weight), unloadBox(weight)
 	// toString
-	
+	public double getCurrentLoad() {
+		return currentLoad;
+	}
+
+	public boolean load(double weight) {
+		// validation rule
+		if (weight <= 0)
+			return false;
+		// business rule
+		if (this.currentLoad + weight > this.capacity)
+			return false;
+		// business logic
+		this.currentLoad += weight;
+		return true;
+	}
+
+	public boolean unload(double weight) {
+		// validation rule
+		if (weight <= 0)
+			return false;
+		// business rule
+		if (weight > this.currentLoad)
+			return false;
+		// business logic
+		this.currentLoad -= weight;
+		return true;
+	}
+
 }
--- lab.session01/test/com/example/domain/VehicleTest.java
+++ lab.session01/test/com/example/domain/VehicleTest.java
@@ -1,14 +1,11 @@
 package com.example.domain;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -22,8 +19,8 @@ class VehicleTest {
 		boolean result = vehicle.load(weight);
 		assertAll(
 				() -> assertTrue(result),
-				() -> assertTrue(currentLoad,vehicle.getCurrentLoad())
-				);
+				() -> assertEquals(currentLoad,vehicle.getCurrentLoad())
+		);
 	}
 
 	@DisplayName("loading weight over the capacity should return false")
@@ -34,8 +31,21 @@ class VehicleTest {
 		boolean result = vehicle.load(weight);
 		assertAll(
 				() -> assertFalse(result),
-				() -> assertTrue(0,vehicle.getCurrentLoad())
+				() -> assertEquals(0,vehicle.getCurrentLoad())
 		);
 	}
 	
+	@ParameterizedTest
+	@CsvFileSource(resources = "unloads_success.csv")
+	@DisplayName("unloading weight under/all current load should return true")
+	void unloadWeightsShouldReturnTrue(double capacity,double loadWeight,double unloadWeight,double currentLoad) {
+		Vehicle vehicle = new Vehicle(capacity);
+		boolean loadResult = vehicle.load(loadWeight);
+		boolean unloadResult = vehicle.unload(unloadWeight);
+		assertAll(
+				() -> assertTrue(loadResult),
+				() -> assertTrue(unloadResult),
+				() -> assertEquals(currentLoad,vehicle.getCurrentLoad())
+		);
+	}
 }
