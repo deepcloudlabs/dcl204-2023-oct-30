ab01f83c14db8eead9bda5e2ab8afc1f218e5685
deepcloudlabs <deepcloudlabs@gmail.com>
2023-11-02 14:16:21 +0300

    initial commit

--- study-stream-api/src/com/example/Exercise01.java
+++ study-stream-api/src/com/example/Exercise01.java
@@ -6,14 +6,23 @@ public class Exercise01 {
 
 	public static void main(String[] args) {
 		var numbers = List.of(4, 8, 15, 16, 23, 42);
+		// imperative programming -> oop
 		var total = 0;
-		for (var number : numbers) {
+		for (var number : numbers) { // external loop
 			if (number%2 == 0) {
 				var cubed = number * number * number;
 				total += cubed;
 			}
 		}
 		System.out.println("total is %d".formatted(total));
+		// declarative programming -> fp
+		total = numbers.stream()  // internal loop
+				       .parallel()
+				       .filter(u -> u%2 == 0)
+				       .map(v -> v*v*v)
+				       .reduce(0, Integer::sum);
+		System.out.println("total is %d".formatted(total));
+				                
 	}
 
 }
---
+++ study-stream-api/src/com/example/Exercise02.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		// Functional Programming
+		// 1. Higher-Order Function: filter/map/reduce
+		// 2. Pure Function: Lambda Expression, Method Reference
+		List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);		
+		var total = numbers.stream()  
+				           .filter( t -> t%2 == 0 )
+				           .map(t -> t*t*t)
+				           .reduce(1, Fun::enBuyuk);
+		System.out.println("total is %d".formatted(total));
+				                
+	}
+
+}
+
+interface Fun{
+	public static int topla(int u,int v) {
+		System.out.println("u=%d,v=%d".formatted(u,v));
+		return u+v;
+	}
+	public static int carp(int u,int v) {
+		System.out.println("u=%d,v=%d".formatted(u,v));
+		return u*v;
+	}
+	public static int enBuyuk(int u,int v) {
+		System.out.println("u=%d,v=%d".formatted(u,v));
+		return u>v ? u : v;
+	}
+}
