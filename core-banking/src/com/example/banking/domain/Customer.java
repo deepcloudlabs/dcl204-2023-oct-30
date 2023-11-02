a9dd6b7e61588a5b9646d9f08d910608bcdd8c6f
deepcloudlabs <deepcloudlabs@gmail.com>
2023-11-02 11:03:59 +0300

    Update Customer.java

--- core-banking/src/com/example/banking/domain/Customer.java
+++ core-banking/src/com/example/banking/domain/Customer.java
@@ -1,91 +1,94 @@
 package com.example.banking.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class Customer {
-    private final String identityNo;
-    private String fullName;
-    private final List<Account> accounts = new ArrayList<>();
+	private final String identityNo;
+	private String fullName;
+	private final Map<String, Account> accounts = new HashMap<>();
+	private final List<Account> accountList = new ArrayList<>();
 
-    // Constructor -> identityNo (getter), fullName(getter/setter)
+	// Constructor -> identityNo (getter), fullName(getter/setter)
 
-    Customer(String identityNo, String fullName) {
-        this.identityNo = identityNo;
-        this.fullName = fullName;
-    }
+	Customer(String identityNo, String fullName) {
+		this.identityNo = identityNo;
+		this.fullName = fullName;
+	}
 
-    //region getter/setter
-    public String getIdentityNo() {
-        return identityNo;
-    }
+	// region getter/setter
+	public String getIdentityNo() {
+		return identityNo;
+	}
 
-    public String getFullName() {
-        return fullName;
-    }
+	public String getFullName() {
+		return fullName;
+	}
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-    //endregion
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	// endregion
 
-    public void addAccount(Account account) {
-        String iban = account.getIban();
-        if (!this.existsAccount(iban))
-            accounts.add(account);
-    }
+	public void addAccount(Account account) {
+		String iban = account.getIban();
+		if (!this.existsAccount(iban)) {
+			this.accounts.put(iban, account);
+			this.accountList.add(account);
+		}
+	}
 
-    public boolean existsAccount(String iban) {
-        for (Account account : accounts) {
-            if (account.getIban().equals(iban))
-                return true;
-        }
-        return false;
-    }
+	public boolean existsAccount(String iban) {
+		return accounts.containsKey(iban);
+	}
 
-    public int getNumberOfAccounts() {
-        return this.accounts.size();
-    }
+	public int getNumberOfAccounts() {
+		return this.accounts.size();
+	}
 
-    public Optional<Account> getAccount(int index) {
-        if (index < 0) return Optional.empty();
-        if (index >= this.accounts.size()) return Optional.empty();
-        return Optional.of(this.accounts.get(index));
-    }
+	public Optional<Account> getAccount(int index) {
+		if (index < 0)
+			return Optional.empty();
+		if (index >= this.accountList.size())
+			return Optional.empty();
+		return Optional.of(this.accountList.get(index));
+	}
 
-    public Optional<Account> getAccount(String iban) {
-        for (Account account : accounts) {
-            if (account.getIban().equals(iban))
-                return Optional.of(account);
-        }
-        return Optional.empty();
-    }
+	public Optional<Account> getAccount(String iban) {
+		return Optional.ofNullable(accounts.get(iban));
+	}
 
-    public List<Account> getAccounts() {
-        return Collections.unmodifiableList(this.accounts);
-    }
+	public List<Account> getAccounts() {
+		return List.copyOf(this.accounts.values());
+	}
 
-    public Optional<Account> removeAccount(String iban) {
-        Optional<Account> foundAccount = getAccount(iban);
-        if (foundAccount.isPresent()) {
-            this.accounts.remove(foundAccount.get());
-        }
-        return foundAccount;
-    }
+	public Optional<Account> removeAccount(String iban) {
+		Optional<Account> foundAccount = getAccount(iban);
+		if (foundAccount.isPresent()) {
+			this.accounts.remove(iban);
+			this.accountList.remove(foundAccount.get());
+		}
+		return foundAccount;
+	}
 
-    public Optional<Account> removeAccount(Account account) {
-        if (this.accounts.remove(account))
-            return Optional.of(account);
-        return Optional.empty();
-    }
+	public Optional<Account> removeAccount(Account account) {
+		final var iban = account.getIban();
+		if (this.accounts.containsKey(iban)) {
+			this.accounts.remove(account.getIban());
+			this.accountList.remove(account);
+			return Optional.of(account);
+		}
+		return Optional.empty();
+	}
 
-    public double getTotalBalance() {
-        double totalBalance = 0.0;
-        for (Account account : accounts) {
-            totalBalance += account.getBalance();
-        }
-        return totalBalance;
-    }
+	public double getTotalBalance() {
+		double totalBalance = 0.0;
+		for (Account account : accounts.values()) {
+			totalBalance += account.getBalance();
+		}
+		return totalBalance;
+	}
 }
