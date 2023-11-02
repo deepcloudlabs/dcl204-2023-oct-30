6398b0a5116e216a86591d7cdb7b6ca72835b91d
deepcloudlabs <deepcloudlabs@gmail.com>
2023-11-02 15:31:56 +0300

    Update Exercise7.java

--- stream-api-exercises-part2/src/com/example/animals/exercises/Exercise7.java
+++ stream-api-exercises-part2/src/com/example/animals/exercises/Exercise7.java
@@ -1,10 +1,15 @@
 package com.example.animals.exercises;
 
+import static java.util.stream.Collectors.groupingBy;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.example.animals.domain.Animal;
 import com.example.animals.domain.Cat;
+import com.example.animals.domain.Centipede;
 import com.example.animals.domain.Fish;
 import com.example.animals.domain.Spider;
 
@@ -15,9 +20,13 @@ import com.example.animals.domain.Spider;
  */
 public class Exercise7 {
 	public static void main(String[] args) {
-		// Count the number of animals in each specie 
+		// Count the number of animals in each species 
 		List<Animal> animals = Arrays.asList(new Cat(), new Spider(), new Cat("Tekir"), new Fish("Free Willy"),
-				new Spider(), new Fish("Jaws"));
-		
+				new Spider(), new Fish("Jaws"), new Centipede());
+		Function<Animal,Class<?>> toClass = Animal::getClass;
+		Function<Class<?>,String> toSimpleName = Class::getSimpleName;
+		var groups = animals.stream()
+				.collect(groupingBy(toClass.andThen(toSimpleName), Collectors.counting()));
+		System.out.println(groups);		
 	}
 }
